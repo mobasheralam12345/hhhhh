85b481c9bcc6ec4633787f75fb50eba4117fd1ca
Mobassher Alam <138420019+mobasheralam12345@users.noreply.github.com>
2024-11-26 16:40:06 +0600

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css';
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Routes';
+import AuthProvider from './provider/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <div className='max-w-screen-xl h-fit mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </QueryClientProvider>
+    </AuthProvider>
+  </React.StrictMode >,
+)
